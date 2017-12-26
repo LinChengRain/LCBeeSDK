187f8dab3c4f959097293c43c6918da8ad58cfd5
yuchanglin <yuchanglinlin@163.com>
2017-12-26 19:03:40 +0800

    first commit,initial project

---
+++ LCBeeSDK/LCBeeSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCBeeSDK
+//
+//  Created by yuchanglin on 2017/12/26.
+//  Copyright © 2017年 yuchanglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
